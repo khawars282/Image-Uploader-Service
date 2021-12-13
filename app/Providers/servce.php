6d26037f0c7c4d56e9c1ff1aa0ec102b6d518d9b
Khawar <khawars282@gmail.com>
2021-12-13 12:13:28 +0500

    first commit

--- app/Providers/servce.php
+++ app/Providers/servce.php
@@ -10,29 +10,35 @@ use Exception;
 
 class servce 
 {
+    // function create token 
     public function createToken($data)
     {
-
+        // get key from .env
         $key = config('constant.secret');
-
+        // get array data from .env 
         $payload = array(
             config('constant.required_jwt_data'),
             "data" => $data,
         );
         try
         {
+            //upload data in jwt token 
              $jwt = JWT::encode($payload,$key, 'HS256');
+             //token
              return $jwt;
         }
          catch(Exception $ex)
          {
+             //response error
              return array('error'=>$ex->getMessage());
          }
     }
+    // function create token 
     public function decodeToken($jwt)
          {
+            // get key from .env
             $key = config('constant.secret'); 
-                 
+            //decoded token
             $decoded = JWT::decode($jwt, new Key($key, 'HS256'));
         return $decoded;
     }
