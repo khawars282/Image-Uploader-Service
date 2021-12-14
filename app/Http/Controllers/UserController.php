971779bf690d84dc2dbec241c2450750a0f66b5e
Khawar <khawars282@gmail.com>
2021-12-14 13:49:59 +0500

    first commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,7 +20,6 @@ use App\Http\Requests\UsersProfile;
 use App\Http\Requests\UsersSendResetLinkRequest;
 use App\Http\Requests\UsersSendResetRequest;
 use App\Providers\servce;
-use App\Http\Middleware\EnsureTokenIsValid;
 use App\Mail\ResetPasswordMail;
 use Illuminate\Support\Facades\Mail;
 
